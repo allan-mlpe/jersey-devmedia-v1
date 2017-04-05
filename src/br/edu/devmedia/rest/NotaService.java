9f608daeba6d8f72b00c44fa8b05142083f25c09
Allan Monteiro <allan_mlpe@hotmail.com>
2017-04-05 19:43:54 -0300

    Implementando do serviço de listagem de notas

--- src/br/edu/devmedia/rest/NotaService.java
+++ src/br/edu/devmedia/rest/NotaService.java
@@ -1,10 +1,45 @@
 package br.edu.devmedia.rest;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.edu.devmedia.dao.NotaDAO;
+import br.edu.devmedia.model.Nota;
+
 /**
  * Contém os endpoints básicos para operações de inserção, exclusão, atualização e recuperação de notas
  * @author Allan
  *
  */
-public class NotaService {
 
+@Path("/notas") //padrão de url dos serviços dessa classe
+public class NotaService {
+	
+	/**
+	 * Objeto DAO para gerenciar o CRUD de notas
+	 */
+	private NotaDAO dao; 
+	
+	/**
+	 * Método executado quando da criação do objeto. A annotation "@PostConstruct" é utilizada
+	 * para evitar a utilização de um construtor, uma vez que é o Jersey que irá gerenciar
+	 * a criação dos objetos de serviços da aplicação.
+	 */
+	@PostConstruct
+	private void init() {
+		dao = new NotaDAO();
+	}
+	
+	@GET //método da requisição HTTP aceito pelo serviço
+	@Path("/") //path do serviço
+	@Produces(MediaType.APPLICATION_JSON) //tipo de dado retornado pelo serviço
+	private List<Nota> listarNotas() {
+		List<Nota> notas = dao.listarNotas();
+		return notas;
+	}
 }
