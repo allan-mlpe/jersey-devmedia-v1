8508e879e92ac589774c73a954584d06b259cc7b
Allan Monteiro <allan_mlpe@hotmail.com>
2017-04-04 21:23:55 -0300

    Criação da classe de serviços de CRUD básico para as notas

---
+++ src/br/edu/devmedia/rest/NotaService.java
@@ -0,0 +1,10 @@
+package br.edu.devmedia.rest;
+
+/**
+ * Contém os endpoints básicos para operações de inserção, exclusão, atualização e recuperação de notas
+ * @author Allan
+ *
+ */
+public class NotaService {
+
+}
